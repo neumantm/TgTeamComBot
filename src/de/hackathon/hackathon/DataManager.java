5e7d0e11fb62da4f2012c9ba0d8234b1fb67476e
Tim <st151270@stud.uni-stuttgart.de>
2017-07-08 13:57:52 +0200

    DataManager and needed chages to Main

--- src/de/hackathon/hackathon/DataManager.java
+++ src/de/hackathon/hackathon/DataManager.java
@@ -9,10 +9,14 @@
  */
 package de.hackathon.hackathon;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 import de.hackathon.hackathon.data.*;
+import de.tim.lib.Log;
 
 /**
  * TODO: Description
@@ -25,8 +29,48 @@ public class DataManager {
 	HashMap<Long, Event> event = new HashMap<>();
 	HashMap<Long, ToDo> toDos = new HashMap<>();
 
-	public void load() {
+	private File dataFolder;
 
+	public DataManager(File p_dataFolder) {
+		if (!p_dataFolder.exists()) {
+			Main.mainLog.log("DataManager is requested to open non existent folder:" + p_dataFolder.getAbsolutePath(), Log.WARN);
+			Main.closeData();
+		}
+
+		if (!p_dataFolder.isDirectory()) {
+			Main.mainLog.log("DataManager is requested to open a file but should be a folder:" + p_dataFolder.getAbsolutePath(), Log.WARN);
+			Main.closeData();
+		}
+
+		this.dataFolder = p_dataFolder;
+
+		load();
+	}
+
+	private void load() {
+		try (ObjectInputStream oIS = new ObjectInputStream(new FileInputStream(new File(this.dataFolder, "bodys.dat")))) {
+			while (true) {
+				Object tmpO = oIS.readObject();
+				if (!(tmpO instanceof Body)) {
+					oIS.close();
+					throw new ClassCastException("WTF! There is a incompatible Object in here.");
+				}
+				Body tmpB = (Body) tmpO;
+				this.bodys.put(tmpB., tmpD);
+			}
+		} catch (EOFException e) {
+			//Ignore
+			return true;
+		} catch (FileNotFoundException e) {
+			Main.getLog().logException(e, Log.ERROR, false);
+			return false;
+		} catch (IOException e) {
+			Main.getLog().logException(e, Log.ERROR, false);
+			return false;
+		} catch (ClassNotFoundException e) {
+			Main.getLog().logException(e, Log.ERROR, false);
+			return false;
+		}
 	}
 
 	public void save(String fileName) {
