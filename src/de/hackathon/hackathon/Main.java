7827c0301bf5f5330773d69464cb45cac8ad4e64
Tim <st151270@stud.uni-stuttgart.de>
2017-07-08 12:01:11 +0200

    Initial Commit

---
+++ src/de/hackathon/hackathon/Main.java
@@ -0,0 +1,26 @@
+/*
+ * hackathon
+ *
+ * TODO: Project Beschreibung
+ *
+ * @author Tim Neumann
+ * @version 1.0.0
+ *
+ */
+package de.hackathon.hackathon;
+
+/**
+ * TODO: Description
+ * @author Tim Neumann
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
