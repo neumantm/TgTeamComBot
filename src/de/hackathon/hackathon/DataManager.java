6f7e92484fd29d27523a69ae6a40af7649243b15
Tim <st151270@stud.uni-stuttgart.de>
2017-07-08 13:31:43 +0200

    Readded DataManager

---
+++ src/de/hackathon/hackathon/DataManager.java
@@ -0,0 +1,36 @@
+/*
+ * hackathon
+ *
+ * TODO: Project Beschreibung
+ *
+ * @author Tim Neumann
+ * @version 1.0.0
+ *
+ */
+package de.hackathon.hackathon;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import de.hackathon.hackathon.data.*;
+
+/**
+ * TODO: Description
+ * 
+ * @author Henne
+ */
+public class DataManager {
+
+	HashMap<Integer, Body> bodys = new HashMap<>();
+	HashMap<Integer, Event> event = new HashMap<>();
+
+	public void load() {
+
+	}
+
+	public void save(String fileName) {
+		try {
+			
+		}
+	}
+}
