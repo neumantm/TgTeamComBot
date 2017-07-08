eec5b1257ee7bd98267dfcc49e2a199e16681642
Tim <st151270@stud.uni-stuttgart.de>
2017-07-08 19:41:46 +0200

    Started with Scheduler.

---
+++ src/de/hackathon/hackathon/bot/PossbileActions.java
@@ -0,0 +1,23 @@
+/*
+ * hackathon
+ *
+ * A telegram Bot that supports Groups, ToDos and events.
+ *
+ * @author Tim Neumann, Fabian Hutzenlaub, Patrick Muerdter
+ * 
+ * @copyright Copyright (c) Tim Neumann, Fabian Hutzenlaub, Patrick Muerdter
+ * @license Licensed under CC BY SA 4.0
+ * 
+ * @version 1.0.0
+ *
+ */
+package de.hackathon.hackathon.bot;
+
+/**
+ * TODO: Description
+ * 
+ * @author Tim Neumann, Fabian Hutzenlaub, Patrick Muerdter
+ */
+public enum PossbileActions {
+	REMIND_TODOS
+}
---
+++ src/de/hackathon/hackathon/scheduler/SchedulerEvent.java
@@ -0,0 +1,49 @@
+/*
+ * hackathon
+ *
+ * A telegram Bot that supports Groups, ToDos and events.
+ *
+ * @author Tim Neumann, Fabian Hutzenlaub, Patrick Muerdter
+ * 
+ * @copyright Copyright (c) Tim Neumann, Fabian Hutzenlaub, Patrick Muerdter
+ * @license Licensed under CC BY SA 4.0
+ * 
+ * @version 1.0.0
+ *
+ */
+package de.hackathon.hackathon.scheduler;
+
+import java.util.Date;
+
+import de.hackathon.hackathon.bot.PossbileActions;
+
+/**
+ * TODO: Description
+ * 
+ * @author Tim Neumann, Fabian Hutzenlaub, Patrick Muerdter
+ */
+public abstract class SchedulerEvent {
+	private Long chatId;
+	private PossbileActions action;
+
+	/**
+	 * Get's {@link #chatId chatId}
+	 * 
+	 * @return chatId
+	 */
+	public Long getChatId() {
+		return this.chatId;
+	}
+
+	/**
+	 * Get's {@link #action action}
+	 * 
+	 * @return action
+	 */
+	public PossbileActions getAction() {
+		return this.action;
+	}
+
+	public abstract Date getNextOccurence();
+
+}
