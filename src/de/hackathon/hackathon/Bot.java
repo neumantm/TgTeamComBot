954197b7615bae26578e79a10d3dc69826d22f57
Tim <st151270@stud.uni-stuttgart.de>
2017-07-08 12:56:26 +0200

    Added Config, Log, Bot skell

---
+++ src/de/hackathon/hackathon/Bot.java
@@ -0,0 +1,50 @@
+/*
+ * hackathon
+ *
+ * TODO: Project Beschreibung
+ *
+ * @author Tim Neumann
+ * @version 1.0.0
+ *
+ */
+package de.hackathon.hackathon;
+
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+
+/**
+ * The bot. It handles the communication with the telegram servers.
+ * 
+ * @author Tim Neumann, Fabian Hutzenlaub, Patrick Muerdter
+ */
+public class Bot extends TelegramLongPollingBot {
+
+	public static final String NAME = "hackathon";
+	public static final String TOKEN = "448728416:AAF4AkOiDUc-zx13pxhAUTH_2kEGsdhrQto";
+
+	/**
+	 * @see org.telegram.telegrambots.generics.LongPollingBot#onUpdateReceived(org.telegram.telegrambots.api.objects.Update)
+	 */
+	@Override
+	public void onUpdateReceived(Update arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * @see org.telegram.telegrambots.generics.LongPollingBot#getBotUsername()
+	 */
+	@Override
+	public String getBotUsername() {
+		return Bot.NAME;
+	}
+
+	/**
+	 * @see org.telegram.telegrambots.bots.DefaultAbsSender#getBotToken()
+	 */
+	@Override
+	public String getBotToken() {
+		return Bot.TOKEN;
+	}
+
+}
--- src/de/hackathon/hackathon/Main.java
+++ src/de/hackathon/hackathon/Main.java
@@ -1,25 +1,68 @@
 /*
  * hackathon
  *
- * TODO: Project Beschreibung
+ * A telegram Bot that supports Groups, ToDos and events.
  *
- * @author Tim Neumann
+ * @author Tim Neumann, Fabian Hutzenlaub, Patrick Muerdter
  * @version 1.0.0
  *
  */
 package de.hackathon.hackathon;
 
+import java.io.IOException;
+import java.util.HashMap;
+
+import de.tim.lib.Config;
+import de.tim.lib.Log;
+
 /**
- * TODO: Description
- * @author Tim Neumann
+ * Main class
+ * 
+ * @author Tim Neumann, Fabian Hutzenlaub, Patrick Muerdter
  */
 public class Main {
+
+	private static final String configLocation = "config.conf";
+
+	/** The main config */
+	public static Config config;
+	/** The main log */
+	public static Log mainLog;
+
+	/**
+	 * Loads the config from disk.
+	 */
+	private static void loadConfig() {
+		HashMap<String, String> configFields = new HashMap<>();
+
+		configFields.put("LogLevel", "INFO");
+
+		try {
+			Main.config = new Config(Main.configLocation, "Main Configuartion file for SteWoDB.", configFields);
+		} catch (IOException e) {
+			Main.mainLog.logException(e, Log.ERROR, true);
+		}
+	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		///Log loading
+		try {
+			Main.mainLog = new Log(new String[] { "stdout", "main.log" }, Log.INFO);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		Main.mainLog.redirectSTDErr();
+
+		//Config
+		Main.loadConfig();
+
+		Main.mainLog.setLogLevel(Log.getLogLevel(Main.config.getConfigValue("LogLevel")));
+
+		Main.mainLog.log("Done loading configs.", Log.INFO);
+		Main.mainLog.log("LogLevel DEBUG enabeled.", Log.DEBUG);
 
 	}
 
